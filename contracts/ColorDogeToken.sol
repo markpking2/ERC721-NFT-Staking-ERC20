6870e52e5b415eb6d7ed332322b2153275d5b2b8
Mark King <markpking2@gmail.com>
2022-08-15 21:27:55 -0400

    add set address, approve all, and stake mint functions to token

--- contracts/ColorDogeToken.sol
+++ contracts/ColorDogeToken.sol
@@ -13,6 +13,18 @@ contract ColorDogeToken is ERC20 {
         _owner = msg.sender;
     }
 
+    function setNFTAddress(address _target) external {
+        require(msg.sender == _owner, "not owner");
+        require(_target != address(0), "0 address");
+        nftAddress = _target;
+    }
+
+    function setNFTStakeAddress(address _target) external {
+        require(msg.sender == _owner, "not owner");
+        require(_target != address(0), "0 address");
+        nftStakeAddress = _target;
+    }
+
     function mint() external payable {
         uint toMint = msg.value * 1000; // 1000 tokens per ether
         require(toMint > 0, "must send at least 2000 wei");
@@ -20,6 +32,25 @@ contract ColorDogeToken is ERC20 {
             revert("Can't mint more than total supply.");
         }
         _mint(msg.sender, toMint);
+    }
+
+    function mint(address _to, uint _amount) public {
+        require(msg.sender == nftStakeAddress, "not nft stake contract");
+        _mint(_to, _amount);
+
+
+    }
+
+    function approveAllNFT(bool approve) external {
+        require(address(nftAddress) != address(0), "NFT address not set");
+        uint amount = approve == true ? type(uint).max : 0;
+        _approve(msg.sender, nftAddress, amount);
+    }
+
+    function approveAllStake(bool approve) external {
+        require(address(nftStakeAddress) != address(0), "Stake address not set");
+        uint amount = approve == true ? type(uint).max : 0;
+        _approve(msg.sender, nftStakeAddress, amount);
     }
 
     fallback() external payable {}
