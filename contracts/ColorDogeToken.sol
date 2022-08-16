14723ca63da848b516ce27ee32f752ca1f539388
Mark King <markpking2@gmail.com>
2022-08-15 21:27:20 -0400

    add mint to token

--- contracts/ColorDogeToken.sol
+++ contracts/ColorDogeToken.sol
@@ -5,12 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 pragma solidity 0.8.9;
 
 contract ColorDogeToken is ERC20 {
-    address public immutable _owner;
+    address immutable public _owner;
     address private nftAddress;
     address private nftStakeAddress;
 
-    constructor() ERC20("Color Doge Token", "CDTK") {
+    constructor() ERC20("Color Doge Token", "CDTK"){
         _owner = msg.sender;
+    }
+
+    function mint() external payable {
+        uint toMint = msg.value * 1000; // 1000 tokens per ether
+        require(toMint > 0, "must send at least 2000 wei");
+        if(totalSupply() + toMint > 100_000_000 * 10**18){
+            revert("Can't mint more than total supply.");
+        }
+        _mint(msg.sender, toMint);
     }
 
     fallback() external payable {}
