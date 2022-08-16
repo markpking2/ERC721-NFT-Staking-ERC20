52906c4692aff4401948563e7fb3b5b6edd4a14f
Mark King <markpking2@gmail.com>
2022-08-15 21:31:08 -0400

    add nft mint, _baseURI, and withdrawTokens functions

--- contracts/ColorDogeNFT.sol
+++ contracts/ColorDogeNFT.sol
@@ -8,13 +8,30 @@ pragma solidity 0.8.9;
 contract ColorDogeNFT is ERC721 {
     uint256 public totalSupply = 0;
     uint256 public constant MAX_SUPPLY = 100;
-    address public immutable _owner;
+    address immutable public _owner;
     IERC20 private immutable colorDogeTokenAddress;
     address nftStakeAddress;
 
     constructor(IERC20 _colorDogeTokenAddress) ERC721("Color Doge", "CDOGE") {
         _owner = msg.sender;
         colorDogeTokenAddress = _colorDogeTokenAddress;
+    }
+
+    function mint(uint256 _tokenId) external {
+        require(totalSupply < MAX_SUPPLY, "supply reached");
+        colorDogeTokenAddress.transferFrom(msg.sender, address(this), 10 * 10**18);
+        _mint(msg.sender, _tokenId);
+        totalSupply++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmNMRKZXodDUANghAZbzQXE551j7bV1w933Y9u3Zhv8AYm/";
+    }
+
+    function withdrawTokens() external onlyOwner {
+        uint currentBalance = colorDogeTokenAddress.balanceOf(address(this));
+        require(currentBalance > 0, "no tokens");
+        colorDogeTokenAddress.transfer(_owner, currentBalance);
     }
 
     fallback() external payable {}
